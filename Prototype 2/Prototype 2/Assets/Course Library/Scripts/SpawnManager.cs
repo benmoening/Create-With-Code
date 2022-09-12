a21776ad9b2a7b1eaaac5e9a7c15bd43fe3af055
benmoening <23moenb@elida.k12.oh.us>
2022-09-12 12:27:00 -0400

    9/9 & 9/12 changes

---
+++ Prototype 2/Prototype 2/Assets/Course Library/Scripts/SpawnManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public float startDelay = 2;
+    public float spawnInterval = 1.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+
+    public GameObject[] animalPrefabs;
+    public float spawnRangeX = 20;
+    public float spawnPosZ = 20;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            // Randomly generate animal index and spawn position
+            SpawnRandomAnimal();
+        }
+    }
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnpos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Instantiate(animalPrefabs[animalIndex], spawnpos, animalPrefabs[animalIndex].transform.rotation);
+    }
+}
