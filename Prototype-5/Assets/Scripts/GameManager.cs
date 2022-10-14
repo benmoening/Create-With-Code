b1752df59f8fc0f10ce5f3fc41a4e6a40f1393af
benmoening <23moenb@elida.k12.oh.us>
2022-10-14 12:27:57 -0400

    10/14
    imported files

---
+++ Prototype-5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public float spawnRate = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    public void OnTriggerEnter (Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
