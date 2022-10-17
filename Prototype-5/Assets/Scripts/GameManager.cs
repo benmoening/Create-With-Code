6cecd1286737cc58ee2a37a5b68a8497f85b5a3d
benmoening <23moenb@elida.k12.oh.us>
2022-10-17 12:26:46 -0400

    10/17

--- Prototype-5/Assets/Scripts/GameManager.cs
+++ Prototype-5/Assets/Scripts/GameManager.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public float spawnRate = 1.0f;
+    public TextMeshProUGUI scoreText;
+    public int score;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
     }
 
     // Update is called once per frame
@@ -26,16 +31,14 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+            
         }
     }
 
-    public void OnMouseDown()
+    public void UpdateScore(int scoreToAdd)
     {
-        Destroy(gameObject);
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
-
-    public void OnTriggerEnter (Collider other)
-    {
-        Destroy(gameObject);
-    }
+    
 }
