bb7350358e7499666d00b25793907ddb23ee9485
benmoening <23moenb@elida.k12.oh.us>
2022-11-07 12:07:18 -0500

    Challenge 5 complete

--- Challenge 5/Assets/Challenge 5/Scripts/GameManagerX.cs
+++ Challenge 5/Assets/Challenge 5/Scripts/GameManagerX.cs
@@ -21,16 +21,26 @@ public class GameManagerX : MonoBehaviour
     private float spaceBetweenSquares = 2.5f; 
     private float minValueX = -3.75f; //  x value of the center of the left-most square
     private float minValueY = -3.75f; //  y value of the center of the bottom-most square
-    
+
+    public float time = 60;
+
     // Start the game, remove title screen, reset score, and adjust spawnRate based on difficulty button clicked
-    public void StartGame()
+    public void StartGame(int difficulty)
     {
-        spawnRate /= 5;
+        spawnRate /= difficulty;
         isGameActive = true;
         StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(0);
         titleScreen.SetActive(false);
+    }
+
+    public void Update()
+    {
+        if (!isGameActive) return;
+        time -= Time.deltaTime;
+        Debug.Log("Time: " + time);
+        if (time <= 0) GameOver();
     }
 
     // While game is active spawn a random target
